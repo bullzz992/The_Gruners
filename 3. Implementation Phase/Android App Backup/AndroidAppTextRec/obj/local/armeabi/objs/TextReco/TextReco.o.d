e1b17f43ea9cae3446df6ff92d9be108e0e8f51b
Mbulungo Justin Musetsho <mbulungom41@gmail.com>
2014-07-28 19:00:59 +0200

    Android App Backup Project.

---
+++ 3. Implementation Phase/Android App Backup/AndroidAppTextRec/obj/local/armeabi/objs/TextReco/TextReco.o.d
@@ -0,0 +1,176 @@
+obj/local/armeabi/objs/TextReco/TextReco.o: jni/TextReco.cpp \
+ D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/jni.h \
+ D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/android/log.h \
+ D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/stdio.h \
+ D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
+ D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
+ D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/android/api-level.h \
+ D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/sys/types.h \
+ D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/stdint.h \
+ D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/sys/_types.h \
+ D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/machine/_types.h \
+ D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/linux/posix_types.h \
+ D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/linux/stddef.h \
+ D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/linux/compiler.h \
+ D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/asm/posix_types.h \
+ D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/asm/types.h \
+ D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/linux/types.h \
+ D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/machine/kernel.h \
+ D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h \
+ D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/string.h \
+ D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/malloc.h \
+ D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/stdlib.h \
+ D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/alloca.h \
+ D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/strings.h \
+ D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/memory.h \
+ D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/assert.h \
+ D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/GLES2/gl2.h \
+ D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/GLES2/gl2platform.h \
+ D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/KHR/khrplatform.h \
+ D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/GLES2/gl2ext.h \
+ jni/../../../build/include/QCAR/QCAR.h \
+ jni/../../../build/include/QCAR/System.h \
+ jni/../../../build/include/QCAR/CameraDevice.h \
+ jni/../../../build/include/QCAR/NonCopyable.h \
+ jni/../../../build/include/QCAR/VideoMode.h \
+ jni/../../../build/include/QCAR/CameraCalibration.h \
+ jni/../../../build/include/QCAR/Vectors.h \
+ jni/../../../build/include/QCAR/Renderer.h \
+ jni/../../../build/include/QCAR/Matrices.h \
+ jni/../../../build/include/QCAR/State.h \
+ jni/../../../build/include/QCAR/Frame.h \
+ jni/../../../build/include/QCAR/VideoBackgroundConfig.h \
+ jni/../../../build/include/QCAR/Trackable.h \
+ jni/../../../build/include/QCAR/TrackableResult.h \
+ jni/../../../build/include/QCAR/Tool.h \
+ jni/../../../build/include/QCAR/Tracker.h \
+ jni/../../../build/include/QCAR/TrackerManager.h \
+ jni/../../../build/include/QCAR/Word.h \
+ jni/../../../build/include/QCAR/Rectangle.h \
+ jni/../../../build/include/QCAR/Area.h \
+ jni/../../../build/include/QCAR/Image.h \
+ jni/../../../build/include/QCAR/WordResult.h \
+ jni/../../../build/include/QCAR/Obb2D.h \
+ jni/../../../build/include/QCAR/TextTracker.h \
+ jni/../../../build/include/QCAR/WordList.h \
+ jni/../../../build/include/QCAR/UpdateCallback.h \
+ jni/../../../build/include/QCAR/DataSet.h jni/SampleUtils.h \
+ jni/LineShaders.h jni/Quad.h
+
+D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/jni.h:
+
+D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/android/log.h:
+
+D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/stdio.h:
+
+D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/sys/cdefs.h:
+
+D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h:
+
+D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/android/api-level.h:
+
+D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/sys/types.h:
+
+D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/stdint.h:
+
+D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/sys/_types.h:
+
+D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/machine/_types.h:
+
+D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/linux/posix_types.h:
+
+D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/linux/stddef.h:
+
+D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/linux/compiler.h:
+
+D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/asm/posix_types.h:
+
+D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/asm/types.h:
+
+D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/linux/types.h:
+
+D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/machine/kernel.h:
+
+D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h:
+
+D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/string.h:
+
+D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/malloc.h:
+
+D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/stdlib.h:
+
+D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/alloca.h:
+
+D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/strings.h:
+
+D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/memory.h:
+
+D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/assert.h:
+
+D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/GLES2/gl2.h:
+
+D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/GLES2/gl2platform.h:
+
+D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/KHR/khrplatform.h:
+
+D:/ProgramFiles/Android/android-ndk-r9/platforms/android-8/arch-arm/usr/include/GLES2/gl2ext.h:
+
+jni/../../../build/include/QCAR/QCAR.h:
+
+jni/../../../build/include/QCAR/System.h:
+
+jni/../../../build/include/QCAR/CameraDevice.h:
+
+jni/../../../build/include/QCAR/NonCopyable.h:
+
+jni/../../../build/include/QCAR/VideoMode.h:
+
+jni/../../../build/include/QCAR/CameraCalibration.h:
+
+jni/../../../build/include/QCAR/Vectors.h:
+
+jni/../../../build/include/QCAR/Renderer.h:
+
+jni/../../../build/include/QCAR/Matrices.h:
+
+jni/../../../build/include/QCAR/State.h:
+
+jni/../../../build/include/QCAR/Frame.h:
+
+jni/../../../build/include/QCAR/VideoBackgroundConfig.h:
+
+jni/../../../build/include/QCAR/Trackable.h:
+
+jni/../../../build/include/QCAR/TrackableResult.h:
+
+jni/../../../build/include/QCAR/Tool.h:
+
+jni/../../../build/include/QCAR/Tracker.h:
+
+jni/../../../build/include/QCAR/TrackerManager.h:
+
+jni/../../../build/include/QCAR/Word.h:
+
+jni/../../../build/include/QCAR/Rectangle.h:
+
+jni/../../../build/include/QCAR/Area.h:
+
+jni/../../../build/include/QCAR/Image.h:
+
+jni/../../../build/include/QCAR/WordResult.h:
+
+jni/../../../build/include/QCAR/Obb2D.h:
+
+jni/../../../build/include/QCAR/TextTracker.h:
+
+jni/../../../build/include/QCAR/WordList.h:
+
+jni/../../../build/include/QCAR/UpdateCallback.h:
+
+jni/../../../build/include/QCAR/DataSet.h:
+
+jni/SampleUtils.h:
+
+jni/LineShaders.h:
+
+jni/Quad.h:
