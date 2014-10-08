6f819215338e053933d921dc45de188bafedd266
Joas Mogale <mpakimogale@gmail.com>
2014-10-08 06:46:02 -0800

    incomplete user manual

--- 3. Implementation Phase/Web Service & DB 4/WebServiceApplication/build/generated-sources/ap-source-output/entity/Detection_.java
+++ 3. Implementation Phase/Web Service & DB 4/WebServiceApplication/build/generated-sources/ap-source-output/entity/Detection_.java
@@ -5,13 +5,14 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.1.v20130918-rNA", date="2014-10-03T03:32:10")
+@Generated(value="EclipseLink-2.5.1.v20130918-rNA", date="2014-10-05T08:27:18")
 @StaticMetamodel(Detection.class)
 public class Detection_ { 
 
     public static volatile SingularAttribute<Detection, Date> date;
     public static volatile SingularAttribute<Detection, String> deviceIp;
     public static volatile SingularAttribute<Detection, String> location;
+    public static volatile SingularAttribute<Detection, String> numberplate;
     public static volatile SingularAttribute<Detection, Integer> featureId;
     public static volatile SingularAttribute<Detection, Integer> detectionId;
 
--- 3. Implementation Phase/Web Service & DB 4/WebServiceApplication/build/generated-sources/ap-source-output/entity/Vehicle_.java
+++ 3. Implementation Phase/Web Service & DB 4/WebServiceApplication/build/generated-sources/ap-source-output/entity/Vehicle_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.1.v20130918-rNA", date="2014-10-03T03:32:10")
+@Generated(value="EclipseLink-2.5.1.v20130918-rNA", date="2014-10-05T08:27:18")
 @StaticMetamodel(Vehicle.class)
 public class Vehicle_ { 
 
